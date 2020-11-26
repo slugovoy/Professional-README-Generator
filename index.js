64a5a7bfd740937a61958b3f6b7c42193034bb13
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2020-11-25 19:31:22 -0700

    changed functions, added async

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-// array of questions for user
+// Variable for array of questions
 const questions = require("./questions");
-const inquirer = require("inquirer");
-const fs = require("fs");
+// Variables for packages
+const { prompt } = require("inquirer");
+const { writeFile } = require("fs/promises");
+// Variable for function from external file
 const generateMarkdown = require("./utils/generateMarkdown");
 
-function writeToFile() {
-inquirer.prompt(questions).then((response) => {
-
-  const readmeTemplate = generateMarkdown(response)
-
-  fs.writeFile(
-    "README.md", readmeTemplate,
-    (err) =>
-      err
-        ? console.log(err)
-        : console.log("Success!Take a look at your README.md")
-  );
-});
-}
-
 // function to initialize program
-function init() {
-  writeToFile();
+async function init() {
+  try {
+    //   Variable to keep promise
+    let response = await prompt(questions);
+    // Variable for generated template
+    const template = generateMarkdown(response);
+   
+    // Write template to file body
+    await writeFile("README'sExample.md", template);
+    console.log("Done!");
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 // function call to initialize program
